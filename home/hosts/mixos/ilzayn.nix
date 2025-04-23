218280c648909f604f6df50f540f623d23c6d10c
Jonas Elhs <jonas.elhs@outlook.com>
2025-04-23 16:01:22 +0200

    walker: added

--- home/hosts/mixos/ilzayn.nix
+++ home/hosts/mixos/ilzayn.nix
@@ -5,6 +5,7 @@
 
   home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink /home/ilzayn/nixos/dotfiles/waybar;
   home.file.".config/hypr".source = config.lib.file.mkOutOfStoreSymlink /home/ilzayn/nixos/dotfiles/hypr;
+  home.file.".config/walker".source = config.lib.file.mkOutOfStoreSymlink /home/ilzayn/nixos/dotfiles/walker;
 
   services.hyprpaper.enable = true;
 
