fed3b395c1d5e7f1df7d70277f3229046fee17b7
Jonas Elhs <jonas.elhs@outlook.com>
2025-05-02 20:06:23 +0200

    kitty: theming

--- home/modules/terminal/kitty.nix
+++ home/modules/terminal/kitty.nix
@@ -33,6 +33,31 @@ in {
           window_padding_width = 8;
           background_blur = 0;
           cursor_trail = 1;
+
+          # Theme
+          foreground = config.theme.colors.foreground;
+          background = config.theme.colors.background;
+          selection_foreground = config.theme.colors.background;
+          selection_background = config.theme.colors.foreground;
+          url_color = "#88C0D0";
+          cursor = config.theme.colors.foreground;
+
+          color0 = config.theme.colors.base00;
+          color1 = config.theme.colors.base01;
+          color2 = config.theme.colors.base02;
+          color3 = config.theme.colors.base03;
+          color4 = config.theme.colors.base04;
+          color5 = config.theme.colors.base05;
+          color6 = config.theme.colors.base06;
+          color7 = config.theme.colors.base07;
+          color8 = config.theme.colors.base08;
+          color9 = config.theme.colors.base09;
+          color10 = config.theme.colors.base0A;
+          color11 = config.theme.colors.base0B;
+          color12 = config.theme.colors.base0C;
+          color13 = config.theme.colors.base0D;
+          color14 = config.theme.colors.base0E;
+          color15 = config.theme.colors.base0F;
         };
       };
     }.${cfg.style};
