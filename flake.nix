d59417be30a6fc404772efba1fe4b76c037d8e7b
Jonas Elhs <jonas.elhs@outlook.com>
2025-05-29 11:15:43 +0200

    flake: update

--- flake.nix
+++ flake.nix
@@ -137,6 +137,22 @@
           ({ ... }: {
             environment.systemPackages = (scripts hosts.${host}.system);
           })
+
+          ({ ... }: {
+            nix.settings = {
+              trusted-users = [
+                "root"
+                "@wheel"
+              ];
+              substituters = [
+                "https://walker-git.cachix.org"
+              ];
+              trusted-public-keys = [
+                "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+              ];
+            };
+          })
+
         ];
       }
     );
