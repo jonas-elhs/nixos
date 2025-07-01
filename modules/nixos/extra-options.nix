6fe5069cda57ed2c0148f1cd3139e99af95f2053
jonas-elhs <jonas.elhs@outlook.com>
2025-07-01 18:53:18 +0200

    scripts: add rebuild scripts

--- modules/nixos/extra-options.nix
+++ modules/nixos/extra-options.nix
@@ -5,5 +5,9 @@
       default = "x86_64-linux";
       description = "The system architecture";
     };
+    config-root = lib.mkOption {
+      type = lib.types.str;
+      description = "An absolute path to the flakes root directory.";
+    };
   };
 }
