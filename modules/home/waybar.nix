4f9e9cd7853ba8009312cf1249037d7f36e8a97d
jonas-elhs <jonas.elhs@outlook.com>
2025-08-24 12:25:27 +0200

    waybar: fix vertical (not complete)

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -34,21 +34,19 @@ in {
           bar = {
             layer = "top";
             position = "top";
-            margin = "20 20 0 20";
+            margin = "${layout.gap.size} ${libx.stringDivide layout.gap.size 2} 0 ${libx.stringDivide layout.gap.size 2}"; 
             exclusive = true;
-            spacing = 0;
 
             modules-left = [
-              "group/apps"
-              "clock"
-              "user"
+                #"group/apps"
+                #"group/information"
+                #"clock"
             ];
             modules-center = [
               "hyprland/workspaces"
             ];
             modules-right = [
               "group/hardware"
-              "group/information"
               "group/power"
             ];
 
@@ -57,7 +55,7 @@ in {
               modules = [ "custom/launcher" "custom/terminal" "custom/browser" "custom/files" "custom/mail" ];
               drawer = {
                 transition-duration = 300;
-                children-class = "app-icon";
+                children-class = "bottom-drawer-sub-icon";
                 transition-left-to-right = true;
               };
             };
@@ -85,46 +83,6 @@ in {
               format = "";
               tooltip-format = "Mail Client";
               on-click = "";
-            };
-
-            clock = {
-              format = "<span color='${accent}'></span> {:%H:%M}";
-              tooltip-format = "{:%A, %d. %B %Y}";
-            };
-
-            user = {
-              format = "<span color='${accent}'></span> {user}";
-            };
-
-            "hyprland/workspaces" = {
-              format = "";
-              on-click = "activate";
-              on-scroll-up = "hyprctl dispatch workspace e-1";
-              on-scroll-down = "hyprctl dispatch workspace e+1";
-            };
-
-            "group/hardware" = {
-              orientation = "inherit";
-              modules = [ "cpu" "custom/gpu" "memory" "disk" ];
-            };
-            cpu = {
-              format = "<span color='${accent}'></span> {usage}%";
-              tooltip-format = "{load}";
-            };
-            "custom/gpu" = {
-              format = "<span color='${accent}'>󰢮</span> {}%";
-              exec = "cat /sys/class/hwmon/hwmon${toString cfg.gpu_hwmon}/device/gpu_busy_percent";
-              tooltip = false;
-              interval = 1;
-            };
-            memory = {
-              format = "<span color='${accent}'></span> {percentage}%";
-              tooltip-format = "{used:0.1f} GiB / {total:0.1f} GiB";
-            };
-            disk = {
-              format = "<span color='${accent}'></span> {percentage_used}%";
-              tooltip-format = "{specific_used:0.1f} GB / {specific_total:0.1f} GB";
-              unit = "GB";
             };
 
             "group/information" = {
@@ -172,12 +130,62 @@ in {
               };
             };
 
+            clock = {
+              format = "<span color='${accent}'></span> {:%H:%M}";
+              tooltip-format = "{:%A, %d. %B %Y}";
+              justify = "center";
+              expand = true;
+            };
+
+            "hyprland/workspaces" = {
+              format = "";
+              on-click = "activate";
+              on-scroll-up = "hyprctl dispatch workspace e+1";
+              on-scroll-down = "hyprctl dispatch workspace e-1";
+            };
+
+            "group/hardware" = {
+              orientation = "inherit";
+              modules = [ "cpu" "custom/gpu" "memory" "disk" ];
+            };
+            cpu = {
+              format = "<span color='${accent}'></span> {usage}%";
+              tooltip-format = "{load}";
+              justify = "center";
+              align = "left";
+              expand = true;
+            };
+            "custom/gpu" = {
+              format = "<span color='${accent}'>󰢮</span> {}%";
+              exec = "cat /sys/class/hwmon/hwmon${toString cfg.gpu_hwmon}/device/gpu_busy_percent";
+              tooltip = false;
+              interval = 1;
+              justify = "center";
+              align = "left";
+              expand = true;
+            };
+            memory = {
+              format = "<span color='${accent}'></span> {percentage}%";
+              tooltip-format = "{used:0.1f} GiB / {total:0.1f} GiB";
+              justify = "center";
+              align = "left";
+              expand = true;
+            };
+            disk = {
+              format = "<span color='${accent}'></span> {percentage_used}%";
+              tooltip-format = "{specific_used:0.1f} GB / {specific_total:0.1f} GB";
+              unit = "GB";
+              justify = "center";
+              align = "left";
+              expand = true;
+            };
+
             "group/power" = {
               orientation = "inherit";
               modules = [ "custom/shutdown" "custom/suspend" "custom/hibernate" "custom/logout" "custom/lock" "custom/reboot" ];
               drawer = {
                 transition-duration = 300;
-                children-class = "power-icon";
+                children-class = "top-drawer-sub-icon";
                 transition-left-to-right = false;
               };
             };
@@ -220,27 +228,23 @@ in {
             font-size: ${text-size}px;
             min-height: 0;
             background: transparent;
-            border-style: none;
+            border: none;
             color: ${text};
           }
 
-          tooltip {
-            background: ${background};
-            border: ${layout.border.width}px solid ${accent};
-          }
-
+          /* Visual Groups */
           #apps,
+          #information,
           #clock,
-          #user,
           #workspaces,
           #hardware,
-          #information,
-          #power {
-            border-radius: ${layout.border.radius.size}px;
-            background: alpha(${background}, 0.9);
-            padding: 0px 10px;
-            margin: 0px ${layout.gap.size}px;
+          #power,
+          tooltip label {
+            background: alpha(${background}, ${toString layout.background.opacity});
+            padding: ${layout.gap.inner}px;
+            margin: 0px ${libx.stringDivide layout.gap.size 2}px;
             border: ${layout.border.width}px solid ${inactive};
+            border-radius: ${layout.border.radius.size}px;
             transition: border 0.2s ease;
           }
           #apps:hover,
@@ -252,41 +256,51 @@ in {
             border: ${layout.border.width}px solid ${accent};
           }
 
-          #apps {
-            margin-left: 0px;
-            padding: 0px 12px;
-          }
-          .app-icon label {
-            font-size: ${sub-size}px;
-            margin: 0px 0px 0px 20px;
+          /* Tooltips Border */
+          tooltip label {
+            border: ${layout.border.width}px solid ${accent};
           }
 
-          #power {
-            margin-right: 0px;
-            padding: 0px 12px;
-          }
-          .power-icon label {
+          /* Drawers */
+          .bottom-drawer-sub-icon label {
             font-size: ${sub-size}px;
-            margin: 0px 20px 0px 0px;
+            margin: 0px 0px 0px ${layout.gap.inner}px;
+          }
+          .top-drawer-sub-icon label {
+            font-size: ${sub-size}px;
+            margin: 0px ${layout.gap.inner}px 0px 0px;
           }
 
+          /* Groups */
+          #hardware,
+          #information {
+            padding: ${layout.gap.inner}px ${libx.stringDivide layout.gap.inner 2}px;
+          }
           #hardware label,
           #information label {
-            margin: 0 10px;
+            margin: 0px ${libx.stringDivide layout.gap.inner 2}px;
           }
 
+          /* None Padded Groups */
+          #hardware {
+            padding: 0px ${libx.stringDivide layout.gap.inner 2}px;
+          }
+
+          /* Workspaces */
+          #workspaces {
+            padding: ${layout.gap.inner}px ${libx.stringDivide layout.gap.inner 2}px;
+          }
           #workspaces button {
             background: ${text};
             padding: 0px 0px;
-            margin: 10px 5px;
+            margin: 3px ${libx.stringDivide layout.gap.inner 2}px;
             transition: padding 0.2s ease;
             border-radius: 10px;
-            border-style: none;
+            border: none;
           }
           #workspaces button:hover {
             background: ${text};
-            border-color: rgba(0, 0, 0, 0);
-            border-style: none;
+            border: none;
             box-shadow: none;
           }
           #workspaces button.active {
@@ -313,8 +327,9 @@ in {
         settings = {
           bar = {
             layer = "top";
-            position = "left";
-            margin = "${libx.stringDivide layout.gap.size 2} 0 ${libx.stringDivide layout.gap.size 2} ${layout.gap.size}";
+            position = "right";
+              #margin = "${libx.stringDivide layout.gap.size 2} 0 ${libx.stringDivide layout.gap.size 2} ${layout.gap.size}";
+            margin = "${libx.stringDivide layout.gap.size 2} ${layout.gap.size} ${libx.stringDivide layout.gap.size 2} 0";
             exclusive = true;
 
             modules-left = [
@@ -365,6 +380,51 @@ in {
               on-click = "";
             };
 
+            "group/information" = {
+              orientation = "inherit";
+              modules = [ "network" "bluetooth" "wireplumber" ];
+            };
+            network = {
+              format-wifi = "<span color='${accent}'>{icon}</span>";
+              tooltip-format-wifi = "{essid} ({signalStrength}%)";
+              format-ethernet = "<span color='${accent}'></span>";
+              tooltip-format-ethernet = "{ifname} ({bandwidthUpBits}  | {bandwidthDownBits} )";
+              format-linked = "<span color='${accent}'></span>";
+              tooltip-format-linked = "{ifname}";
+              format-disconnected = "";
+              format-icons = [
+                "󰤯"
+                "󰤟"
+                "󰤢"
+                "󰤥"
+                "󰤨"
+              ];
+            };
+            bluetooth = {
+              format-disabled = "";
+              format-off = "<span color='${accent}'>󰂲</span>";
+              format-on = "<span color='${accent}'>󰂯</span>";
+              format-connected = "<span color='${accent}'>󰂯</span>";
+              tooltip-format = "{num_connections} Devices";
+              tooltip-format-off = "Off";
+              on-click = "bluetooth-toggle";
+            };
+            wireplumber = {
+              format = "<span color='${accent}'>{icon}</span>";
+              format-low = "<span color='${accent}'>x</span> {volume}%"; # not working
+              format-none = "<span color='${accent}'>x</span> {volume}%"; # not working
+              format-muted = "<span color='${accent}'></span>";
+              tooltip-format = "{volume}%";
+              on-click = "audio-toggle";
+
+              format-icons = [ "" "" "" ];
+
+              states = {
+                none = 0;
+                low = 50;
+              };
+            };
+
             clock = {
               format = "<span color='${accent}'></span>\n{:%H\n%M}";
               tooltip-format = "{:%A, %d. %B %Y}";
@@ -413,51 +473,6 @@ in {
               justify = "center";
               align = "left";
               expand = true;
-            };
-
-            "group/information" = {
-              orientation = "inherit";
-              modules = [ "network" "bluetooth" "wireplumber" ];
-            };
-            network = {
-              format-wifi = "<span color='${accent}'>{icon}</span>";
-              tooltip-format-wifi = "{essid} ({signalStrength}%)";
-              format-ethernet = "<span color='${accent}'></span>";
-              tooltip-format-ethernet = "{ifname} ({bandwidthUpBits}  | {bandwidthDownBits} )";
-              format-linked = "<span color='${accent}'></span>";
-              tooltip-format-linked = "{ifname}";
-              format-disconnected = "";
-              format-icons = [
-                "󰤯"
-                "󰤟"
-                "󰤢"
-                "󰤥"
-                "󰤨"
-              ];
-            };
-            bluetooth = {
-              format-disabled = "";
-              format-off = "<span color='${accent}'>󰂲</span>";
-              format-on = "<span color='${accent}'>󰂯</span>";
-              format-connected = "<span color='${accent}'>󰂯</span>";
-              tooltip-format = "{num_connections} Devices";
-              tooltip-format-off = "Off";
-              on-click = "bluetooth-toggle";
-            };
-            wireplumber = {
-              format = "<span color='${accent}'>{icon}</span>";
-              format-low = "<span color='${accent}'>x</span> {volume}%"; # not working
-              format-none = "<span color='${accent}'>x</span> {volume}%"; # not working
-              format-muted = "<span color='${accent}'></span>";
-              tooltip-format = "{volume}%";
-              on-click = "audio-toggle";
-
-              format-icons = [ "" "" "" ];
-
-              states = {
-                none = 0;
-                low = 50;
-              };
             };
 
             "group/power" = {
