8f9a3d2c3eec369ddf69cd98fa994486d6fad703
jonas-elhs <jonas.elhs@outlook.com>
2025-11-02 09:29:43 +0100

    ilzayn/cursor: scale down

--- hosts/mixos/users/ilzayn.nix
+++ hosts/mixos/users/ilzayn.nix
@@ -64,13 +64,9 @@
   home.pointerCursor = {
     enable = true;
 
-    # MacOS Cursors
-    # name = "macOS";
-    # package = pkgs.apple-cursor;
-
-    # Phinger Cursors
     name = "phinger-cursors-dark";
     package = pkgs.phinger-cursors;
+    size = 24;
 
     gtk.enable = true;
     x11.enable = true;
